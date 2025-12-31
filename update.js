0bab2f676a8d2ece40601b131339e7636bb420ca
beckett <91700492+beckettclarke@users.noreply.github.com>
2025-12-31 17:55:18 -0500

    EOL warning, final update

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-var latest = 12;
+var latest = 14;
 var wni1t = 'Update checker';
 var wni1s = 'cloud-arrow-up';
 var wni2t = 'Autoclicker';
@@ -11,12 +11,15 @@ var wni3s = 'bolt';
 if(window.location.href.includes('?update=1')){
     var urlParams = new URLSearchParams(window.location.search);
     var current = urlParams.get('version');
+    var currentNum = current !== null ? Number(current) : NaN;
     var lvs = document.querySelectorAll('.latestversion');
     lvs.forEach(function(e){e.innerHTML=latest})
     var cvs = document.querySelectorAll('.currentversion');
     cvs.forEach(function(e){e.innerHTML=current})
-    if(window.location.href.includes('&version=' + latest)){
+    if(!isNaN(currentNum) && currentNum === latest){
         document.getElementById('uptodate').style.bottom="0px";
+    } else if(!isNaN(currentNum) && currentNum > latest){
+        document.getElementById('betaversion').style.bottom="0px";
     } else {
         document.getElementById('whatsnew').innerHTML = '<i class="fal fa-' + wni1s + '"></i>' + wni1t + '<br><i class="fal fa-' + wni2s + '"></i>' + wni2t + '<br><i class="fal fa-' + wni3s + '"></i>' + wni3t
         document.getElementById('oldversion').style.bottom="0px";
